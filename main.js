1c899418ca8e94c534b67e7f1df9aa96b597b88d
imagazine-member <imagazine-member@gmail.com>
2022-11-14 18:08:23 +0900

    del about opacity&slide

--- main.js
+++ main.js
@@ -29,38 +29,38 @@ function scrollIntoView(selector) {
 }
 
 //Make home slowly fade to transparent as the window scrolls down
-const about = document.querySelector('.about__textBox');
-const aboutHeight = about.getBoundingClientRect().height;
-document.addEventListener('scroll', () => {
-    about.style.opacity = 1-scrollY/aboutHeight;
-})
+// const about = document.querySelector('.about__textBox');
+// const aboutHeight = about.getBoundingClientRect().height;
+// document.addEventListener('scroll', () => {
+//     about.style.opacity = 1-scrollY/aboutHeight;
+// })
 
 
 //slide format
-const slides = document.querySelector('.contents__slide');
-const slideImg = document.querySelectorAll('.contents__slide div');
-let currentIdx = 0;
-const slideCount = slideImg.length;
-const prev = document.querySelector('.prev');
-const next = document.querySelector('.next');
-const slideWidth = 300;
-const slideMargin = 100;
+// const slides = document.querySelector('.contents__slide');
+// const slideImg = document.querySelectorAll('.contents__slide div');
+// let currentIdx = 0;
+// const slideCount = slideImg.length;
+// const prev = document.querySelector('.prev');
+// const next = document.querySelector('.next');
+// const slideWidth = 300;
+// const slideMargin = 100;
 
-slides.style.width = (slideWidth + slideMargin) * slideCount + 'px';
+// slides.style.width = (slideWidth + slideMargin) * slideCount + 'px';
 
-function moveSlide(num) {
-    slides.style.left = -num * 400 + 'px';
-    currentIdx = num;
-}
+// function moveSlide(num) {
+//     slides.style.left = -num * 400 + 'px';
+//     currentIdx = num;
+// }
 
-prev.addEventListener('click', () => {
-    if(currentIdx !==0) {
-        moveSlide(currentIdx-1);
-    }
-});
+// prev.addEventListener('click', () => {
+//     if(currentIdx !==0) {
+//         moveSlide(currentIdx-1);
+//     }
+// });
 
-next.addEventListener('click', () => {
-    if(currentIdx !== slideCount -1) {
-        moveSlide(currentIdx +1);
-    }
-})
+// next.addEventListener('click', () => {
+//     if(currentIdx !== slideCount -1) {
+//         moveSlide(currentIdx +1);
+//     }
+// })
